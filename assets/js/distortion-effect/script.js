942414ca55c8278e8581b1cd9715a256d11ae2c2
Andrew Vasilchenko <cazonove@gmail.com>
2024-01-24 00:38:39 +0300

    fix

--- assets/js/distortion-effect/script.js
+++ assets/js/distortion-effect/script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			imagesRatio: 347 / 257,
 			image1: images[0].getAttribute('src'),
 			image2: images[1].getAttribute('src'),
-			displacementImage: `assets/images/distortion-effect/1.png`,
+			displacementImage: `assets/images/distortion-effect/21.jpg`,
 		});
 
 		const canvas = item.querySelector('canvas')
@@ -40,14 +40,111 @@ document.addEventListener('DOMContentLoaded', () => {
 				}
 			}
 		});
-		
+
 		observer.observe(canvas, { attributes: true });
 	})
 	/** (End) Hover Distortion Effect **/
 
 	/** (Start) Slider Distortion Effect **/
 
-	
+	const distortionEffectSliders = document.querySelectorAll('.slider-distortion-effect')
+
+	if (distortionEffectSliders.length > 0) {
+		distortionEffectSliders.forEach((slider) => {
+			const btnPrev = slider.querySelector('[data-nav="previous"]'),
+				btnNext = slider.querySelector('[data-nav="next"]'),
+				sliderWrapper = slider.querySelector('.slider-wrapper'),
+				images = sliderWrapper.querySelectorAll('img'),
+				interval = slider.dataset.intervalTransition
+				
+			const arraySlide = {};
+			const arraySrcImage = []
+
+			let currentIndex = 0
+
+			let aspectRatioY = images[0].naturalHeight
+			let aspectRatioX = images[0].naturalWidth
+
+			images.forEach((image) => {
+				const srcImage = image.getAttribute('src')
+				arraySrcImage.push(srcImage)
+			})
+
+			arraySrcImage.forEach((src, index) => {
+				const nextIndex = (index + 1) % arraySrcImage.length;
+				const key = `distortionEffect${index + 1}`;
+				arraySlide[key] = new hoverEffect({
+					parent: sliderWrapper,
+					intensity: 0.3,
+					imagesRatio: aspectRatioY / aspectRatioX,
+					image1: arraySrcImage[nextIndex],
+					image2: src,
+					displacementImage: 'assets/images/distortion-effect/21.jpg',
+					hover: false,
+				});
+			});
+
+			let canvases,
+				isAnimate
+
+			function nextDistortionEffect() {
+				if (isAnimate) {
+					return;
+				}
+				stopAutoNext()
+				isAnimate = true
+				let prevIndex = currentIndex
+				currentIndex = (currentIndex + 1) % arraySrcImage.length
+				const prevKey = `distortionEffect${prevIndex + 1}`;
+				arraySlide[prevKey].next();
+				setTimeout(() => {
+					canvases = sliderWrapper.querySelectorAll('canvas')
+					sliderWrapper.appendChild(canvases[0])
+					arraySlide[prevKey].previous();
+					isAnimate = false
+					startAutoNext()
+				}, 600)
+			}
+
+			function prevDistortionEffect() {
+				if (isAnimate) {
+					return;
+				}
+				stopAutoNext()
+				isAnimate = true
+				currentIndex = currentIndex - 1 < 0 ? arraySrcImage.length - 1 : currentIndex - 1
+				const currentKey = `distortionEffect${currentIndex + 1}`;
+    		arraySlide[currentKey].next();
+				setTimeout(() => {
+					canvases = sliderWrapper.querySelectorAll('canvas')
+					sliderWrapper.insertBefore(canvases[canvases.length - 1], sliderWrapper.firstChild)
+					arraySlide[currentKey].previous();
+					isAnimate = false
+					startAutoNext()
+				}, 1200)
+			}
+
+			btnNext.addEventListener('click', () => {
+				nextDistortionEffect()
+			})
+
+			btnPrev.addEventListener('click', () => {
+				prevDistortionEffect()
+			})
+
+			let intervalId;
+
+			function startAutoNext() {
+				intervalId = setInterval(nextDistortionEffect, interval);
+			}
+
+			function stopAutoNext() {
+				clearInterval(intervalId);
+			}
+
+			startAutoNext();
+		})
+	}
 
 	/** (End) Slider Distortion Effect **/
 
--- components/comics/comics.js
+++ components/comics/comics.js
@@ -10,48 +10,48 @@ document.addEventListener('DOMContentLoaded', () => {
   //   autoplaySpeed: 5400,
   //   cssEase: 'linear'
   // });
-  $('.comics__gridWrapper-grid-item--2').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: false,
-    arrows: false,
-    fade: true,
-    autoplay: true,
-    autoplaySpeed: 4800,
-    cssEase: 'linear'
-  });
-  $('.comics__gridWrapper-grid-item--3').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: false,
-    arrows: false,
-    fade: true,
-    autoplay: true,
-    autoplaySpeed: 7200,
-    cssEase: 'linear'
-  });
-  $('.comics__gridWrapper-grid-item--4').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: false,
-    arrows: false,
-    fade: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    cssEase: 'linear'
-  });
-  $('.comics__gridWrapper-grid-item--5').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: false,
-    arrows: false,
-    fade: true,
-    autoplay: true,
-    autoplaySpeed: 6300,
-    cssEase: 'linear'
-  });
+  // $('.comics__gridWrapper-grid-item--2').slick({
+  //   infinite: true,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   dots: false,
+  //   arrows: false,
+  //   fade: true,
+  //   autoplay: true,
+  //   autoplaySpeed: 4800,
+  //   cssEase: 'linear'
+  // });
+  // $('.comics__gridWrapper-grid-item--3').slick({
+  //   infinite: true,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   dots: false,
+  //   arrows: false,
+  //   fade: true,
+  //   autoplay: true,
+  //   autoplaySpeed: 7200,
+  //   cssEase: 'linear'
+  // });
+  // $('.comics__gridWrapper-grid-item--4').slick({
+  //   infinite: true,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   dots: false,
+  //   arrows: false,
+  //   fade: true,
+  //   autoplay: true,
+  //   autoplaySpeed: 5000,
+  //   cssEase: 'linear'
+  // });
+  // $('.comics__gridWrapper-grid-item--5').slick({
+  //   infinite: true,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   dots: false,
+  //   arrows: false,
+  //   fade: true,
+  //   autoplay: true,
+  //   autoplaySpeed: 6300,
+  //   cssEase: 'linear'
+  // });
 })
